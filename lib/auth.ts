e94f4e94c738f4bdf361a2bb222560d4768d4459
Vinit Parekh <vinitrevaliya@gmail.com>
2023-04-18 22:32:20 +0530

    auth fixing

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,11 +1,16 @@
 import{ NextAuthOptions } from 'next-auth';
+import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
 import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, TWITTER_CLIENT_ID, TWITTER_CLIENT_SECRET } from '../config';
 import GoogleProvider from 'next-auth/providers/google';
 import TwitterProvider from 'next-auth/providers/twitter';
+import clientPromise from './mongoClient';
 
 export const AuthOptions: NextAuthOptions = {
     pages: {
         signIn: '/login'
+    },
+    session: {
+        strategy: "jwt",
     },
     providers: [
         GoogleProvider({
@@ -14,7 +19,14 @@ export const AuthOptions: NextAuthOptions = {
         }),
         TwitterProvider({
             clientId: TWITTER_CLIENT_ID ||'',
-            clientSecret: TWITTER_CLIENT_SECRET || ''
+            clientSecret: TWITTER_CLIENT_SECRET || '',
         })
     ],
+    adapter: MongoDBAdapter(clientPromise, {
+        databaseName: 'gptbot',
+        collections: {
+            Accounts: 'account',
+            Users: 'auth-user'
+        }
+    }),
 }
