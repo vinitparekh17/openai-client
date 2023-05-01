662e97d336bb880e473a908aa194261f48e52c40
Vinit Parekh <vinitrevaliya@gmail.com>
2023-05-01 20:39:15 +0530

    backend uri init

--- config/index.ts
+++ config/index.ts
@@ -2,7 +2,8 @@ export const {
     GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET,
     TWITTER_CLIENT_ID,
-    TWITTER_CLIENT_SECRET, 
-    JWT_SECRET, 
-    BACKEND_URI
- } = process.env;
+    TWITTER_CLIENT_SECRET,
+    JWT_SECRET
+} = process.env;
+
+export const BACKEND_URI = 'https://api.omnisive.technetic.co.in/api'
--- utils/auth.ts
+++ utils/auth.ts
@@ -1,44 +1,46 @@
 import type { FormValues } from "../types/auth";
+import { BACKEND_URI } from "../config";
 
 export const authSubmit = async (data: FormValues): Promise<any> => {
     try {
         const { formType } = data
-        await fetch(`https://api.omnisive.technetic.co.in/api/user/${formType}`, {
+        await fetch(`${BACKEND_URI}/user/${formType}`, {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
                 "Accept": "application/json",
             },
             body: JSON.stringify(data),
-            credentials: "include",
+            credentials: "include"
         }).then(res => res.json())
-        .then(data => {
-            if (data.token) {
-                const { token } = data
-                localStorage.setItem("token", token)
-                if(localStorage.getItem("token")){
-                    window.location.href = "/conversations";
+            .then(data => {
+                if (data.token) {
+                    const { token } = data
+                    localStorage.setItem("token", token)
+                    if (localStorage.getItem("token")) {
+                        window.location.href = "/conversations";
+                    }
                 }
-            }
             })
     } catch (error) {
         console.log(error);
     }
 }
 
-export const authSignOut = async (): Promise<any> => {
+export const authSignOut = (): void => {
     try {
-        await fetch("http://54.250.183.65:8080/api/user/signout", {
+        fetch(`${BACKEND_URI}/user/signout`, {
             method: "GET",
             headers: {
                 "Content-Type": "application/json",
                 "Accept": "application/json",
             },
             credentials: "include",
-        }).then(res => res.json())
+        })
+            .then(res => res.json())
             .then(data => {
                 if (data.message) {
-                    localStorage.removeItem("token")
+                    localStorage.removeItem("token");
                     window.location.href = "/login";
                 }
             })
