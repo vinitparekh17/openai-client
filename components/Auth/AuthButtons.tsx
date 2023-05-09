6d57a200500a65be2b7e43a24f707166dd82b9bb
Vinit Parekh <vinitrevaliya@gmail.com>
2023-05-09 20:42:28 +0530

    auth fixed

--- components/Auth/AuthButtons.tsx
+++ components/Auth/AuthButtons.tsx
@@ -10,7 +10,7 @@ const AuthButtons: NextPage = () => {
         <div className="mt-4 flex justify-center">
             <button
                 type="button"
-                onClick={() => signIn('google', { callbackUrl: '/dashboard' })}
+                onClick={() => signIn('google', { callbackUrl: '/conversations' })}
                 className="flex justify-center items-center mr-2 w-full rounded-md border border-none bg-gray-200 px-4 py-4 text-base font-semibold text-gray-700 transition-all duration-200 hover:bg-white focus:bg-white focus:text-black focus:outline-none dark:text-black">
                 <FcGoogle className="h-5 w-5 mr-2" />
                 <span className="uppercase">Continue with Google</span>
