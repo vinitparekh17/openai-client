238e8e447d253235372ee1025df90a2fc8b91157
Vinit Parekh <vinitrevaliya@gmail.com>
2023-05-10 17:44:20 +0530

    bot

--- components/Basic/Navbar.tsx
+++ components/Basic/Navbar.tsx
@@ -43,7 +43,7 @@ export default function Navbar({ setOpen, open }: NavbarProps) {
                     </div>
                     <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                         <div className="flex flex-shrink-0 items-center">
-                            <h1 className={`${Playfair.className} ml-12 text-2xl text-white dark:text-gray-200`}>Omnisive</h1>
+                            <h1 className={`${Playfair.className} sm:ml-12 text-2xl text-white dark:text-gray-200`}>Omnisive</h1>
                         </div>
                     </div>
                     <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
