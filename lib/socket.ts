6dab1249a7144188bde60ad37c2249b871902326
Vinit Parekh <vinitrevaliya@gmail.com>
2023-06-14 21:36:32 +0530

    socket bug

--- lib/socket.ts
+++ lib/socket.ts
@@ -3,5 +3,6 @@ import { BACKEND_URI } from "../config";
 import type { ServerToClientEvents, ClientToServerEvents } from "../types/socket";
 
 export const SocketIo: Socket<ServerToClientEvents, ClientToServerEvents> = io(BACKEND_URI, {
-    transports: ['websocket']
+    transports: ['websocket'],
+    secure: true,
 });
