bc7f220866e1f6277c544fac3c118a2d0d99345b
Vinit Parekh <vinitrevaliya@gmail.com>
2023-06-19 23:21:14 +0530

    chat hook introduced

--- components/Chat/ChatContainer.tsx
+++ components/Chat/ChatContainer.tsx
@@ -1,99 +1,15 @@
-import { useRef, useEffect, useState } from "react";
-import { useSelector } from "react-redux";
-import { getConversation } from "../../utils/chat";
-import type { MutableRefObject } from "react";
-import { SocketClient } from "../../lib/socket";
-import type { Socket } from "socket.io-client";
 import ChatForm from "./ChatForm";
 import Message from "./Message";
-import type { ChunkObj, MessageList, OldMessage } from "../../types";
-import { CurrentAuthState } from "../../slices/authSlice";
-import { BACKEND_URI } from "../../config";
+import { useChat } from "../../hooks/useChat";
 
 export default function ChatContainer() {
-  const socket = useRef() as MutableRefObject<Socket>;
-  const { id } = useSelector(CurrentAuthState);
-  const [isFinished, setIsFinished] = useState(false);
-  const [resChunks, setResChunks] = useState<string[]>([]);
-  const [messages, setMessages] = useState<MessageList>([]);
-
-  useEffect(() => {
-    if (!socket.current) {
-      socket.current = SocketClient(BACKEND_URI, {
-        transports: ["websocket"],
-        secure: true,
-      });
-      socket.current.on("connect", () => {
-        console.log("socket created", socket.current);
-      });
-    }
-    return () => {
-      if (socket.current && socket.current.connected) {
-        socket.current.close();
-      }
-    }
-  }, []);
-
-  useEffect(() => {
-      if (socket.current && socket.current.connected) {
-        console.log("connected to socket");
-        socket.current.on("response-stream", data => {
-          try {
-            let parsed: ChunkObj = JSON.parse(data);
-            if (parsed.data) {
-              setIsFinished(false)
-              setResChunks(prev => [...prev, parsed!.data.choices[0].delta?.content!]);
-            } else {
-              setIsFinished(true)
-            }
-          } catch (e) {
-            console.log(`error: ${e}\n str: ${JSON.stringify(data)}\n data: ${data}`);
-          }
-        });
-      }
-  }, [socket, setIsFinished, setResChunks, isFinished, resChunks]);
-
-  useEffect(() => {
-    if (isFinished) {
-      setTimeout(() => {
-        setMessages(prev => [...prev, { username: "bot", fromself: false, content: resChunks.join(""), timestamp: new Date().toLocaleString() }])
-        setIsFinished(false)
-        setResChunks([])
-      }, 1500)
-    }
-  }, [isFinished, setMessages, setIsFinished, setResChunks, resChunks])
-
-  useEffect(() => {
-    if (messages.length === 0) {
-      getConversation(id)
-        .then(res => res.json())
-        .then(d => {
-          let { data } = d as { data: OldMessage[] }
-          data.map((d: OldMessage) => {
-            setMessages(prev => [...prev, {
-              username: "Vinit",
-              content: d.prompt,
-              fromself: true,
-              timestamp: new Date().toLocaleString()
-            }])
-            setMessages(prev => [...prev, {
-              username: "Bot",
-              content: d.answer,
-              fromself: false,
-              timestamp: new Date().toLocaleString()
-            }])
-          })
-        })
-        .catch(err => console.log(err))
-    }
-
-  }, [messages, id, setMessages])
-
+  const { messages, resChunks, setMessages, socket, setResChunks, messageEndRef } = useChat();
   return (
     <div className="flex flex-col h-full items-center justify-between container py-2">
-      <div className="container px-3 overflow-y-scroll hiddenscroll">
+      <div className="flex flex-col container px-3 overflow-y-scroll hiddenscroll">
         {messages.length > 0 && messages.map((message, i) => <Message key={i} message={message} />)}
         {resChunks.length > 0 && <Message chunks={resChunks} isFinished setResChunks={setResChunks} />}
+      <div className="flex-1" ref={messageEndRef}></div>
       </div>
       <ChatForm socket={socket} setMessages={setMessages} />
     </div>
---
+++ hooks/useChat.ts
@@ -0,0 +1,98 @@
+import { useEffect, useRef, useState } from 'react';
+import { SocketClient } from "../lib/socket";
+import { useSelector } from 'react-redux';
+import { CurrentAuthState } from '../slices/authSlice';
+import { MutableRefObject } from 'react';
+import type { ChunkObj } from '../types';
+import type { Socket } from 'socket.io-client';
+import { getConversation } from '../utils/chat';
+import type { MessageList, OldMessage } from '../types';
+import { BACKEND_URI } from '../config';
+
+export function useChat() {
+  const socket = useRef<Socket>() as MutableRefObject<Socket>;
+  const messageEndRef = useRef<HTMLDivElement>(null);
+  const { id } = useSelector(CurrentAuthState);
+  const [isFinished, setIsFinished] = useState(false);
+  const [resChunks, setResChunks] = useState<string[]>([]);
+  const [messages, setMessages] = useState<MessageList>([]);
+
+  useEffect(() => {
+    if (messages.length === 0) {
+      getConversation(id)
+        .then(res => res.json())
+        .then(d => {
+          let { data } = d as { data: OldMessage[] }
+          data.map((d: OldMessage) => {
+            setMessages(prev => [...prev, {
+              username: "Vinit",
+              content: d.prompt,
+              fromself: true,
+              timestamp: new Date().toLocaleString()
+            }])
+            setMessages(prev => [...prev, {
+              username: "Bot",
+              content: d.answer,
+              fromself: false,
+              timestamp: new Date().toLocaleString()
+            }])
+          })
+        })
+        .catch(err => console.log(err))
+    }
+
+  }, [messages, setMessages])
+
+  useEffect(() => {
+    if (messageEndRef.current) {
+      (messageEndRef.current as any).scrollIntoView({ behavior: 'smooth' });
+    }
+    socket.current = SocketClient(BACKEND_URI, {
+      transports: ["websocket"],
+    })
+    socket.current.on("connect", () => {
+      console.log("connected");
+    });
+    return () => {
+      if(socket.current.connected) {
+      socket.current?.disconnect();
+      }
+    };
+  }, []);
+
+  useEffect(() => {
+    if (socket.current && socket.current.connected) {
+      socket.current.on("response-stream", data => {
+        try {
+          let parsed: ChunkObj = JSON.parse(data);
+          if (parsed.data) {
+            setIsFinished(false)
+            setResChunks(prev => [...prev, parsed!.data.choices[0].delta?.content!]);
+          } else {
+            setIsFinished(true)
+          }
+        } catch (e) {
+          console.log(`error: ${e}\n str: ${JSON.stringify(data)}\n data: ${data}`);
+        }
+      });
+    }
+  }, [socket.current, setResChunks, setIsFinished]);
+
+  useEffect(() => {
+    if (isFinished) {
+      setTimeout(() => {
+        setMessages(prev => [...prev, { username: "bot", fromself: false, content: resChunks.join(""), timestamp: new Date().toLocaleString() }])
+        setIsFinished(false)
+        setResChunks([])
+      }, 1500)
+    }
+  }, [isFinished, setMessages, setIsFinished, setResChunks, resChunks])
+
+  useEffect(() => {
+    if (messageEndRef.current) {
+      (messageEndRef.current as any).scrollIntoView({ behavior: 'smooth' });
+    }
+  }, [messages, messageEndRef.current])
+
+  return { resChunks, messages, setMessages, setResChunks, isFinished, socket, messageEndRef };
+}
